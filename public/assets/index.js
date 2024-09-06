8dfbf8c47e76b1ef1bb2ed20fc13ff571657d214
i-am-lmi0 <arnav.somaghatta@icloud.com>
2024-09-06 17:24:18 -0400

    Create index.js

---
+++ public/assets/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const server = app.listen(8080, () => {
+    console.log(`Server running on port ${server.address().port}`);
+});
+app.use(express.static('public')) // ok what???? 
+console.log("imported pages")
+app.use(express.static('img')) // Yes i can keep it in public but i dont care 
+console.log("imported img")
+console.log("static imports finished")
+// static imports are don i think
